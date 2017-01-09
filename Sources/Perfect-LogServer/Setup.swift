1c269ed50281ec394c974191b01bff9af5691ad7
Jonathan Guthrie <jono@perfect.org>
2017-01-09 16:43:13 -0500

    create default user on install.

--- Sources/Perfect-LogServer/Setup.swift
+++ Sources/Perfect-LogServer/Setup.swift
@@ -7,6 +7,7 @@
 //
 
 import PerfectTurnstilePostgreSQL
+import TurnstileCrypto
 
 func setup() {
 
@@ -14,6 +15,22 @@ func setup() {
 	// Set up the Authentication table
 	let auth = AuthAccount()
 	try? auth.setup()
+
+	// Make sure there is at least 1 user:
+	let usersCount = AuthAccount()
+	try? usersCount.findAll()
+	if usersCount.results.cursorData.totalRecords == 0 {
+		// make default user:
+		let user = AuthAccount()
+		user.username = "admin"
+		user.firstname = "Admin"
+		user.lastname = "Admin"
+		user.password = BCrypt.hash(password: "perfect")
+		let random: Random = URandom()
+		user.id(String(random.secureToken))
+		try? user.create()
+	}
+
 
 	// Connect the AccessTokenStore
 	tokenStore = AccessTokenStore()
